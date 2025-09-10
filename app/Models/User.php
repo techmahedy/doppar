e892e84e77de89288cc40ebb16711445587095d6
Arif Hoque <arif@sulaco-tec.com>
2025-09-10 16:49:33 +0600

    code commented:

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,13 +7,6 @@ use Phaseolies\Database\Eloquent\Model;
 class User extends Model
 {
     /**
-     * The primary key associated with the table.
-     *
-     * @var string
-     */
-    protected $primaryKey = "id";
-
-    /**
      * The table associated with the model.
      *
      * @var string
@@ -33,18 +26,4 @@ class User extends Model
      * @var array
      */
     protected $unexposable = ["password", "remember_token", "two_factor_secret", "two_factor_recovery_codes"];
-
-    /**
-     * Default page size for pagination
-     *
-     * @var int
-     */
-    protected $pageSize = 10;
-
-    /**
-     * Indicates whether the model should maintain timestamps
-     *
-     * @var bool
-     */
-    protected $timeStamps = true;
 }
