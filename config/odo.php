97d61c6f17905b0b9b07acdac4b6450c49355459
Arif Hoque <arif@sulaco-tec.com>
2025-11-22 17:34:46 +0600

    Introducing [odo] template engine for doppar (

---
+++ config/odo.php
@@ -0,0 +1,78 @@
+
+<?php
+
+/**
+ * =========================================
+ * ODO TEMPLATE ENGINE CONFIGURATION
+ * =========================================
+ *
+ * This configuration file allows you to customize the syntax
+ * of odo template engine beyond standard syntax.
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Directive Prefix
+    |--------------------------------------------------------------------------
+    |
+    | The character used to prefix directives. Default to '#'.
+    | You can change this to any single character like '#', '$', '%', '~', etc.
+    |
+    | Example: '#if', '$foreach', '%include'
+    |
+    */
+    'directive_prefix' => '#',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Echo Tags
+    |--------------------------------------------------------------------------
+    |
+    | Configure the opening and closing tags for outputting variables.
+    |
+    | Example: [[ $variable ]]
+    |
+    */
+    'open_echo' => '[[',
+    'close_echo' => ']]',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Raw Echo Tags
+    |--------------------------------------------------------------------------
+    |
+    | Configure tags for unescaped output (raw HTML).
+    |
+    | Example: [[! $html !]]
+    |
+    */
+    'open_raw_echo' => '[[!',
+    'close_raw_echo' => '!]]',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Escaped Echo Tags
+    |--------------------------------------------------------------------------
+    |
+    | Configure tags for explicitly escaped output.
+    |
+    | Example: [[[ $variable ]]]
+    |
+    */
+    'open_escaped_echo' => '[[[',
+    'close_escaped_echo' => ']]]',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Comment Tags
+    |--------------------------------------------------------------------------
+    |
+    | Configure tags for template comments.
+    |
+    | Example: [[-- This is a comment --]]
+    |
+    */
+    'open_comment' => '[[--',
+    'close_comment' => '--]]',
+];
